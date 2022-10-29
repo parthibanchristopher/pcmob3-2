b84fb2d626b745adf00f290f3210b5be67891591
parthibanchristopher <parthibanchristopher@gmail.com>
2022-10-29 14:51:02 +0800

    set up notes page

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Entypo } from "@expo/vector-icons";
+import * as SQLite from 'expo-sqlite';
+
+function NotesScreen({navigation}) {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity>
+          <Entypo 
+            name="new-message"
+            size={24}
+            color="black"
+            style={{ marginRight: 20}}
+          />
+        </TouchableOpacity>
+      )
+    })
+  })
+
+  return <View style={styles.container}></View>
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+          name="Notes" 
+          component={NotesScreen} 
+          options={{
+            headerTitle: "Notes App",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              fontSize: 30,
+            },
+            headerStyle: {
+              height: 120,
+              backgroundColor: "yellow",
+              borderBottomColor: "#ccc",
+              borderBottomWidth: 1,
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffc',
     alignItems: 'center',
     justifyContent: 'center',
   },
